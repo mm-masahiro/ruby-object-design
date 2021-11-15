b6cb590abf0bbf67124aee56e7688565285912e7
mm-masahiro <hand5.mm9.mzh@gmail.com>
2021-11-15 23:43:05 +0900

    fix

--- chapter3/gear.rb
+++ chapter3/gear.rb
@@ -3,14 +3,21 @@ class Gear
 	attr_reader :chainring, :cog, :wheel
 
 	# def initialize(chainring, cog, rim, tire)
+	# def initialize(args)
+	# 	args = defaults.merge(args)
+	# 	# @chainring = chainring
+	# 	# @cog = cog
+	# 	# @wheel = Wheel.new(rim, tire)
+	# 	# @rim = rim
+	# 	# @tire = tire
+	# 	@chainring = args[:chainring]
+	# 	@cog = args[:cog]
+	# 	@wheel = args[:wheel]
+	# end
+
 	def initialize(args)
-		# @chainring = chainring
-		# @cog = cog
-		# @wheel = Wheel.new(rim, tire)
-		# @rim = rim
-		# @tire = tire
-		@chainring = args[:chainring]
-		@cog = args[:cog]
+		@chainring = args.fetch(:chainring, 40)
+		@cog = args.fetch(:cog, 18)
 		@wheel = args[:wheel]
 	end
 
@@ -30,6 +37,10 @@ class Gear
 
 	def wheel
 		@wheel ||= Wheel.new(rim, tire)
+	end
+
+	def defaults
+		{:chainring => 40, :cog => 18}
 	end
 end
 
